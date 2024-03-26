8e643d76f8f693c1394a3683ff2b37a7387ec51c
game4eyes <darkjjun68@gmail.com>
2024-03-26 17:33:27 +0900

    Add project

---
+++ dobby_music/src/main/java/com/project/side/music/DobbyMusicApplication.java
@@ -0,0 +1,13 @@
+package com.project.side.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobbyMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DobbyMusicApplication.class, args);
+    }
+
+}
---
+++ dobby_music/src/test/java/com/project/side/music/DobbyMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.side.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DobbyMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
