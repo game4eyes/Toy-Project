10f98cfe8d14dd696e420cc7989e281d2d9a3aa7
dbaudwns24 <dbaudwns24@gmail.com>
2024-03-27 13:31:24 +0900

    리액트 폴더

---
+++ react/toyproject/src/App.js
@@ -0,0 +1,14 @@
+/*eslint-disable*/
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
