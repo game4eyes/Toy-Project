aacd456af2817fa582fae85916c510ea8b011e75
namung08 <namung08@gmail.com>
2024-03-29 09:19:19 +0900

    feat: 앨범 검색
    [에스파 -> 에스파의 앨범 출력]

--- dobby_music/src/main/java/com/project/side/music/spotifyapi/search/SearchTest.java
+++ dobby_music/src/main/java/com/project/side/music/spotifyapi/search/SearchTest.java
@@ -3,8 +3,11 @@ package com.project.side.music.spotifyapi.search;
 import com.project.side.music.spotifyapi.token.CreateToken;
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Album;
+import com.wrapper.spotify.model_objects.specification.AlbumSimplified;
 import com.wrapper.spotify.model_objects.specification.Artist;
 import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.requests.data.search.simplified.SearchAlbumsRequest;
 import com.wrapper.spotify.requests.data.search.simplified.SearchArtistsRequest;
 import org.apache.hc.core5.http.ParseException;
 
@@ -18,12 +21,12 @@ public class SearchTest {
     // 검색어 초기 세팅
     // 아티스트 검색어
     private static String searchArtistKey = "에스파";
+    // 앨범 초기 검색어
+    private static String searchAlbumKey = "";
     // api access setting
     private static SpotifyApi spotifyApi = new SpotifyApi.Builder().setAccessToken(CreateToken.accesstoken()).build();
-    private static SearchArtistsRequest searchArtistsRequest = spotifyApi.searchArtists(searchArtistKey)
-            // 검색 결과 제한
-            .limit(10)
-            .build();
+    private static SearchArtistsRequest searchArtistsRequest = spotifyApi.searchArtists(searchArtistKey).build();
+    private static SearchAlbumsRequest searchAlbumsRequest = spotifyApi.searchAlbums(searchAlbumKey).build();
     // 아티스트로 검색
     public static String searchArtists_Sync(String searchArtistKey) {
         String resultItems = "";
@@ -38,8 +41,21 @@ public class SearchTest {
         // searchArtistKey로 검색한 결과를 resultItems 에 담아 리턴해줌
         return resultItems;
     }
+    // 아티스트 이름으로 검색
+    // 그 아티스트의 앨범 반환
+    public static String searchAlbums_Sync(String searchAlbumKey) {
+        String result = "";
+        searchAlbumsRequest = spotifyApi.searchAlbums(searchAlbumKey).build();
+        try {
+            final Paging<AlbumSimplified> albumPaging = searchAlbumsRequest.execute();
+            result = Arrays.toString(albumPaging.getItems());
+        } catch (SpotifyWebApiException| ParseException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-        System.out.println(searchArtists_Sync("마마무"));
+        System.out.println(searchAlbums_Sync("에스파"));
     }
 }
