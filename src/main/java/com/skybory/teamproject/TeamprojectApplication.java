1f8877fd651cb98f514c25c62f794a10b9d96260
skybory <xodus4194@gmail.com>
2024-03-31 19:45:54 +0900

    login updating

---
+++ src/main/java/com/skybory/teamproject/TeamprojectApplication.java
@@ -0,0 +1,13 @@
+package com.skybory.teamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamprojectApplication.class, args);
+	}
+
+}
