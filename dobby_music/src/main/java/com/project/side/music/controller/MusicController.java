928c7a98ad6a4740f293939f44cffd676410902a
namung08 <namung08@gmail.com>
2024-03-31 22:43:39 +0900

    feat: api 기능 추가
    [주소 창에 /api/Tracks/aespa/1 이런식 으로 검색하면 에스파의 1번째 트랙 정보가 나옴 하지만 불러 올때 마다 추가되어서 이점을 고쳐야함]

--- dobby_music/src/main/java/com/project/side/music/controller/MusicController.java
+++ dobby_music/src/main/java/com/project/side/music/controller/MusicController.java
@@ -1,9 +1,18 @@
 package com.project.side.music.controller;
 
-import org.springframework.stereotype.Controller;
+import com.project.side.music.service.TrackService;
+import com.wrapper.spotify.model_objects.specification.Track;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class MusicController {
-
-
+    @GetMapping("/api/Tracks/{name}/{num}")
+    public Track[] searchTracksAllResult(@PathVariable("name") String name, @PathVariable("num") Integer num) {
+        return TrackService.searchTrack_Sync(name,num);
+    }
 }
