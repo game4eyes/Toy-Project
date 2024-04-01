9c3fe1dd0942081ee033c6a9fa0e30d28e493ed8
game4eyes <darkjjun68@gmail.com>
2024-04-01 11:02:59 +0900

    feat musicdetail

--- react/toyproject/src/page/MusicDetail.jsx
+++ react/toyproject/src/page/MusicDetail.jsx
@@ -2,13 +2,33 @@ import React from 'react';
 
 import './css/MusicDetail.css';
 
+const albumData = {
+    albumTitle: "Wilting",
+    artistName: "Glass Trio",
+    tracks: [
+      { name: "Thank God It's Friday", duration: "3:53" },
+      { name: "I Can Always Tell", duration: "3:23" },
+      { name: "Back When", duration: "4:03" },
+
+    ]
+  };
+
 function MusicDetail(){
     return(
         <div className="musicDetailContainer">
-            <h2 className="trackTitle">트랙 제목</h2>
-            <p className="artistName">아티스트 이름</p>
-            <div className="playButton">재생</div>
-        </div>
+        <h2 className="trackTitle">{albumData.albumTitle}</h2>
+        <p className="artistName">{albumData.artistName}</p>
+        <div className="trackList">
+        {albumData.tracks.map((track, index) => (
+          <div key={index} className="trackItem">
+            <span className="trackName">{track.name}</span>
+            <span className="trackDuration">{track.duration}</span>
+          </div>
+        ))}
+      </div>
+      <div className="playButton">재생</div>
+    </div>
     );
+    
 }
 export default MusicDetail;
