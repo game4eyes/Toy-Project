a5f8186e031daa63f55dc145af304fbbc06c2cf4
jyto <jyto0322@naver.com>
2024-04-01 23:42:30 +0900

    populateUI함수, fetchProfile함수 채워넣기

--- react/toyproject/src/component/GetProfile.jsx
+++ react/toyproject/src/component/GetProfile.jsx
@@ -60,12 +60,28 @@ const Script = () => {
     }
 
     async function fetchProfile(token) {
-        // 웹 API 불러와서 프로필 새로고침하기
-    }
+        const result = await fetch("https://api.spotify.com/v1/me", {
+            method: "GET", headers: { Authorization: `Bearer ${token}` }
+        });
+    
+        return await result.json();
+    }       // FetchAPI를 사용하도록 호출
 
     function populateUI(profile) {
-        // 위에서 불러온 프로필 데이터에 맞춰서 UI 새로고침하기
-    }
+        document.getElementById("displayName").innerText = profile.display_name;
+        if (profile.images[0]) {
+            const profileImage = new Image(200, 200);
+            profileImage.src = profile.images[0].url;
+            document.getElementById("avatar").appendChild(profileImage);
+            document.getElementById("imgUrl").innerText = profile.images[0].url;
+        }
+        document.getElementById("id").innerText = profile.id;
+        document.getElementById("email").innerText = profile.email;
+        document.getElementById("uri").innerText = profile.uri;
+        document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+        document.getElementById("url").innerText = profile.href;
+        document.getElementById("url").setAttribute("href", profile.href);
+    }       // 프로필 가져온거 표시
 
     return (
         <div>
