5d9e08b5688a76cd275b0328e92471e27e9f5500
dbaudwns24 <dbaudwns24@gmail.com>
2024-04-04 18:25:38 +0900

    feat mypage css

--- react/toyproject/src/page/Mypage.jsx
+++ react/toyproject/src/page/Mypage.jsx
@@ -23,49 +23,49 @@ function Mypage() {
             <div className="mypage-menu-container">
                 <div className='billingLine' >
                     <div className="billing-box">
-                        <div>요금제</div>
+                        <div>내 요금제</div>
                         <h2>Premium 개인</h2>
                         <p>다음 달에는 ! 에 KRW가 청구됩니다.</p>
                         <p>마지막 숫자가ㅁㅁㅁㅁ인 !! 카드</p>
                     </div>
                     <div className="profile-and-card-container">
                         <div className="edit-profile-box">
-                            <BiPencil size={25}/>
+                            <BiPencil size={25} />
                             <div>프로필 편집</div>
                         </div>
                         <div className="update-credit-card-box">
                             <CiCreditCard1 size={25} />
-                            <h5>신용카드 업데이트</h5>
+                            <div>신용카드 업데이트</div>
                         </div>
                     </div>
                 </div>
                 <div className='menu-item'>
-                    <div>
+                    <div className='menu-item-line'>
                         <h4>계정</h4>
-                        <slot><BiPencil />기본 콘텐츠 <GoChevronRight /></slot>
-                        <slot><TbRestore />플레이리스트 복구하기 <GoChevronRight /></slot>
+                        <div className="row"><BiPencil  style={{ backgroundColor: '#2b2a2a' }}/>기본 콘텐츠 <GoChevronRight className='rightIcon' /></div>
+                        <div className="row"><TbRestore style={{ backgroundColor: '#2b2a2a' }}/>플레이리스트 복구하기 <GoChevronRight className='rightIcon' /></div>
                     </div>
-                    <div>
+                    <div className='menu-item-line'>
                         <h4>정액제</h4>
-                        <slot><GrDiamond />이용 가능한 요금제 <GoChevronRight /></slot>
-                        <slot><GrCurrency />요금제 관리하기 <GoChevronRight /></slot>
+                        <div className="row"><GrDiamond style={{ backgroundColor: '#2b2a2a' }}/>이용 가능한 요금제 <GoChevronRight className='rightIcon' /></div>
+                        <div className="row"><GrCurrency style={{ backgroundColor: '#2b2a2a' }}/>요금제 관리하기 <GoChevronRight className='rightIcon' /></div>
                     </div>
-                    <div>
+                    <div className='menu-item-line'>
                         <h4>결제</h4>
-                        <slot><GrNotes />주문 내역 <GoChevronRight /></slot>
-                        <slot><GoCreditCard />저장된 결제카드 <GoChevronRight /></slot>
-                        <slot><AiFillTag />사용하기 <GoChevronRight /></slot>
+                        <div className="row"><GrNotes style={{ backgroundColor: '#2b2a2a' }}/>주문 내역 <GoChevronRight className='rightIcon' /></div>
+                        <div className="row"><GoCreditCard style={{ backgroundColor: '#2b2a2a' }}/>저장된 결제카드 <GoChevronRight className='rightIcon' /></div>
+                        <div className="row"><AiFillTag style={{ backgroundColor: '#2b2a2a' }}/>사용하기 <GoChevronRight className='rightIcon' /></div>
                     </div>
-                    <div>
+                    <div className='menu-item-line'>
                         <h4>보안 및 개인정보 보호 <GoChevronRight /></h4>
-                        <slot><AiOutlineBars />앱 관리 <GoChevronRight /></slot>
-                        <slot><FiBell />알림설정 <GoChevronRight /></slot>
-                        <slot><AiOutlineEye />개인정보 설정 <GoChevronRight /></slot>
-                        <slot><FiTrello />로그인 방식 수정 <GoChevronRight /></slot>
-                        <slot><FiLock />기기 비밀번호 설정하기 <GoChevronRight /></slot>
-                        <slot><AiOutlineApi />어디서든 로그아웃하기 <GoChevronRight /></slot>
+                        <div className="row" ><AiOutlineBars  style={{ backgroundColor: '#2b2a2a' }} />앱 관리 <GoChevronRight className='rightIcon' /></div>
+                        <div className="row"><FiBell style={{ backgroundColor: '#2b2a2a' }} />알림설정 <GoChevronRight className='rightIcon' /></div>
+                        <div className="row"><AiOutlineEye style={{ backgroundColor: '#2b2a2a' }} />개인정보 설정 <GoChevronRight className='rightIcon' /></div>
+                        <div className="row"><FiTrello style={{ backgroundColor: '#2b2a2a' }}/>로그인 방식 수정 <GoChevronRight className='rightIcon' /></div>
+                        <div className="row"><FiLock style={{ backgroundColor: '#2b2a2a' }}/>기기 비밀번호 설정하기 <GoChevronRight className='rightIcon' /></div>
+                        <div className="row"><AiOutlineApi style={{ backgroundColor: '#2b2a2a' }}/>어디서든 로그아웃하기 <GoChevronRight className='rightIcon' /></div>
                     </div>
-                    <div>
+                    <div className='menu-item-line'>
                         <h4>도움말</h4>
                     </div>
                 </div>
