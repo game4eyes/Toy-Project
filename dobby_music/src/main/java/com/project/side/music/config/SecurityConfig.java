3c6ba98bfac02e6b526b125db8e7484c0ee28dd3
임현민 <120604k@gmail.com>
2024-04-06 21:02:05 +0900

    완료

--- dobby_music/src/main/java/com/project/side/music/config/SecurityConfig.java
+++ dobby_music/src/main/java/com/project/side/music/config/SecurityConfig.java
@@ -1,23 +1,21 @@
-//package com.project.side.music.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//        http.authorizeHttpRequests(auth -> auth
-//            .requestMatchers("/", "/login").permitAll()   //모든 사용자가 접근 가능
+package com.project.side.music.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth -> auth
+            .requestMatchers("/").permitAll()   //모든 사용자가 접근 가능
 //            .requestMatchers("/admin").hasRole("ADMIN")     // admin만 사용 가능
 //            .requestMatchers("/my_page/**").hasAnyRole("ADMIN", "USER") .anyRequest().authenticated() // 개인정보는 사용자 및 관리자만 접속 가능
-//            );
-//
-//        return http.build();
-//    }
-//}
+            );
+
+        return http.build();
+    }
+}
