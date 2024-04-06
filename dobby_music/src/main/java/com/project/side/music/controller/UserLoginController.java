ee120fe1c2efb54594f7d8ad47b08d5e25dc54c4
임현민 <120604k@gmail.com>
2024-04-06 20:23:28 +0900

    웹 페이지 나옴

--- dobby_music/src/main/java/com/project/side/music/controller/UserLoginController.java
+++ dobby_music/src/main/java/com/project/side/music/controller/UserLoginController.java
@@ -1,17 +1,14 @@
 package com.project.side.music.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 public class UserLoginController {
 
-    @RestController
-    public class LoginController {
-
-        @GetMapping("/")
-        public String loginUser() {
-			return "Logins";
-        }
+    @GetMapping("/")
+    public String loginUser() {
+        return "Login";
 
     }
 
